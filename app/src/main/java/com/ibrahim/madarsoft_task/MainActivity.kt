7cf7ad31307b6b9348538210c3b3dc2fcd3a6f4d
ibrahim <imardnily30@gmail.com>
2026-02-23 13:06:58 +0200

    add navigation AppNavHost

--- app/src/main/java/com/ibrahim/madarsoft_task/MainActivity.kt
+++ app/src/main/java/com/ibrahim/madarsoft_task/MainActivity.kt
@@ -6,24 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ibrahim.madarsoft_task.ui.theme.Madarsoft_taskTheme
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.ibrahim.madarsoft_task.ui.input.InputScreen
+import com.ibrahim.madarsoft_task.ui.display.DisplayScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Madarsoft_taskTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavHost(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +38,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavHost(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "input") {
+        composable(
+            route = "input?userId={userId}",
+            arguments = listOf(navArgument("userId") { type = NavType.LongType; defaultValue = -1L })
+        ) { backStackEntry ->
+            val userId = backStackEntry.arguments?.getLong("userId") ?: -1L
+            InputScreen(
+                onSaved = { navController.navigate("display") },
+                userId = if (userId > 0L) userId else null,
+                onNavigateToDisplay = { navController.navigate("display") }
+            )
+        }
+        composable("display") {
+            DisplayScreen(onBack = { navController.navigateUp() }, onEdit = { id ->
+                navController.navigate("input?userId=$id")
+            }, onAdd = {
+                navController.navigate("input")
+            })
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     Madarsoft_taskTheme {
-        Greeting("Android")
     }
 }
